86d1c74972db8a794900cf3fbc4cce0d274dc4da
Damian Kolacki <dasmixovski@gmail.com>
2024-05-30 18:15:30 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+class Board:
+
+
+    def __init__(self, size, ships_num):
+        self.size = size
+        self.board = [["[ ]" for x in range(size)] for y in range(size)]
+        self.num_ships = num_ships
+        
+
+
+
+
+
+def new_game():
+
+    size = 5
+    num_ships = 5
+    print("." * 35)
+    print("Welcome to Battleships Game!")
+    print("." * 35)
+    print("Board size: {size}. Number of ships: {ships} ")
